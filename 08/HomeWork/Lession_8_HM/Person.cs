23af213ca29fde272e016437fb03ff9f9e93dee0
Николай Панов <nickpps12@gmail.com>
2019-12-13 02:19:27 +0300

    HomeWork

---
+++ 08/HomeWork/Lession_8_HM/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lession_8_HM
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int AgePlusFour
+        {
+            get { return Age + 4; }
+        }
+
+        public string STR
+        {
+            get { return $"Имя: {Name}. Этому человеку, через четыре года, исполниться: {AgePlusFour}."; }
+        }
+    }
+}
---
+++ 08/HomeWork/Lession_8_HM/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Lession_8_HM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[3];
+
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i] = new Person();
+                enter:
+                Console.Write($"Введите имя {i}-го человека: ");
+                person[i].Name = Console.ReadLine();
+                
+                Console.Write($"Введите возраст {i}-го человека: ");
+                try
+                {
+                    person[i].Age = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"Возраст должен являться целым числом, попробуйте еще раз!\nНажмите любую клавишу, чтобы повторить ввод."); Console.ResetColor();
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto enter;
+                }
+                
+            }
+
+            for (int i = 0; i < person.Length; i++)
+            {
+                Console.WriteLine(person[i].STR);
+            }
+            Console.ReadKey();
+        }
+    }
+}
